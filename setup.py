5d0d6984f598ba52e42a82f0020453ece1cdd7bc
Sushant Gautam <susant.gautam@gmail.com>
2026-02-12 23:02:23 +0000

    Add chonkie[genie] dependency to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         "pydantic>=2.0.0",
         "tiktoken>=0.5.0",
         "python-dotenv>=1.0.0",
+        "chonkie[genie]>=1.5.5",
     ],
     entry_points={
         "console_scripts": [
